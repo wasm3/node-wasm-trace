0a992cc4ac45fca60bb6d64dd2ed08de115df813
Volodymyr Shymanskyy <vshymanskyi@gmail.com>
2020-02-24 04:21:41 +0200

    Post-processing + instrumentation improvements

--- wasm-trace.js
+++ wasm-trace.js
@@ -6,19 +6,35 @@
  * Author: Volodymyr Shymanskyy
  *
  * TODO:
- * - Post-processing
+ * - Switch RAW format to CSV
+ * - Instrument
+ *   - Calls to imported functions
+ *   - Arguments and return values
+ * - Post-process
+ *   - Collapse chain of loops (display count)
+ *   - Collapse empty function {} to a single line
  */
 
 const fs = require("fs");
+const chalk = require("chalk");
+const assert = require('assert').strict;
+const readline = require('readline');
+const stream = require('stream');
 
 const Wasi = require("@wasmer/wasi");
 const Node = require("@wasmer/wasi/lib/bindings/node");
 const Binaryen = require("binaryen");
 
+/*
+ * Arguments
+ */
+
 const argv = require("yargs")
     .usage("$0 [options] <file> [args..]")
-    .example('$0 -E ./test/hello.wasm', 'Instrument, run and trace WASI app')
-    .example('$0 -ELM -i fib ./test/fib32.wasm 20', 'Instrument, run and trace plain wasm file')
+    .example('$0 -E ./test/hello.wasm',                         'Instrument, run and trace WASI app')
+    .example('$0 -ELM --invoke=fib ./test/fib32.wasm 20',       'Instrument, run and trace plain wasm file')
+    .example('$0 ./instrumented.wasm',                          'Run pre-instrumented wasm file')
+    .example('$0 --process=trace.raw.log ./instrumented.wasm',  'Just process an existing raw trace file')
     .option({
       // Instrumentation options
       "execution": {
@@ -51,13 +67,23 @@ const argv = require("yargs")
         default: "trace.log",
         nargs: 1
       },
-      "wasm-output": {
+      "save-wasm": {
         type: "string",
-        describe: "Output instrumented wasm to ...",
+        describe: "Save instrumented wasm to ...",
+        nargs: 1
+      },
+      "save-raw": {
+        type: "string",
+        describe: "Save raw log file to ...",
         nargs: 1
       },
 
       // Other options
+      "process": {
+        type: "string",
+        describe: "Process raw log file",
+        nargs: 1
+      },
       "invoke": {
         alias: "i",
         type: "string",
@@ -72,7 +98,38 @@ const argv = require("yargs")
     .wrap(null)
     .argv;
 
-//console.log(argv)
+/*
+ * Helpers
+ */
+
+function readLines(input) {
+  const output = new stream.PassThrough({ objectMode: true });
+  const rl = readline.createInterface({ input });
+  rl.on("line", line => {
+    output.write(line);
+  });
+  rl.on("close", () => {
+    output.push(null);
+  });
+  return output;
+}
+
+function fatal(msg) {
+  console.error(chalk.grey('[tracer] ') + chalk.red.bold("Error: ") + msg);
+  process.exit(1);
+}
+
+function warn(msg) {
+  console.error(chalk.grey('[tracer] ') + chalk.yellow.bold("Warning: ") + msg);
+}
+
+function log(msg) {
+  console.error(chalk.grey('[tracer] ') + msg);
+}
+
+/*
+ * Main
+ */
 
 (async () => {
   const inputFile = argv._[0]
@@ -81,7 +138,14 @@ const argv = require("yargs")
   try {
     binary = fs.readFileSync(inputFile);
   } catch (e) {
-    fatal(`File ${inputFile} not found.`);
+    fatal(`File ${inputFile} not found`);
+  }
+
+  // If just post-processing is needed
+  if (argv.process) {
+    const raw = fs.createReadStream(argv.process);
+    await post_process(binary, raw, argv);
+    return;
   }
 
   argv.args = argv._.slice(1);
@@ -89,8 +153,8 @@ const argv = require("yargs")
 
   binary = await instrument(binary, argv);
 
-  if (argv.wasmOutput) {
-    fs.writeFileSync(argv.wasmOutput, binary);
+  if (argv.saveWasm) {
+    fs.writeFileSync(argv.saveWasm, binary);
   }
 
   /* I64 transform is not needed (as soon as we're running with wasm-bigint support)
@@ -100,17 +164,17 @@ const argv = require("yargs")
   */
 
   await execute(binary, argv)
+
+  const raw = fs.createReadStream(".wasm-trace.raw");
+  await post_process(binary, raw, argv);
+
+  // Cleanup
+  if (argv.saveRaw) {
+    fs.renameSync(".wasm-trace.raw", argv.saveRaw)
+  } else {
+    fs.unlinkSync(".wasm-trace.raw")
+  }
 })();
-
-
-function fatal(msg) {
-  console.error(`[tracer] Error: ${msg}`);
-  process.exit(1);
-}
-
-function log(msg) {
-  console.error(`[tracer] ${msg}`);
-}
 
 async function analyze_wasm(binary)
 {
@@ -126,9 +190,12 @@ async function analyze_wasm(binary)
   }
 
   const instrument_funcs = [
-    "log_execution",
-    "load_ptr", "store_ptr",
-    "get_i32", "set_i32",
+    "log_execution",          // log-execution
+    "log_exec_enter",         // log-execution-ex
+    "log_exec_exit",
+    "log_exec_loop",
+    "load_ptr", "store_ptr",  // instrument-memory
+    "get_i32", "set_i32",     // instrument-locals
     "get_i64", "set_i64",
     "get_f32", "set_f32",
     "get_f64", "set_f64",
@@ -155,36 +222,49 @@ async function analyze_wasm(binary)
 
 async function instrument(binary, opts)
 {
-  let passes = []; //"flatten"
-
-  if (opts.execution) passes.push("log-execution");
-  if (opts.locals)    passes.push("instrument-locals");
-  if (opts.memory)    passes.push("instrument-memory");
-
-  if (0 == passes.length) {
+  const workNeeded = (opts.locals || opts.memory || opts.execution);
+  if (workNeeded && opts._wasmInfo.isInstrumented) {
+    fatal(`Wasm file seems to be already instrumented => refusing to instrument`)
+  } else if (!workNeeded) {
     if (!opts._wasmInfo.isInstrumented) {
-      log(`Warning: No instrumentation was applied. Consider using -E, -L, -M flags`)
+      warn(`No instrumentation was applied (consider using -E, -L, -M flags)`)
     }
     return binary;
   }
 
-  if (opts._wasmInfo.isInstrumented) {
-    fatal(`Wasm file seems to be already instrumented. Refusing to instrument: ${passes}.`)
+  if (opts.optimize) {
+    log("Instrumenting and optimizing...")
+  } else {
+    log("Instrumenting...")
   }
-
-  log("Instrumenting...")
 
   Binaryen.setDebugInfo(true);
   let module = Binaryen.readBinary(binary);
-  module.runPasses(passes);
 
+  // 1. instrument-locals, instrument-memory, ...
+  if (opts.locals) {
+    module.runPasses(["instrument-locals"]);
+  }
+  if (opts.memory) {
+    module.runPasses(["instrument-memory"]);
+  }
+
+  // 3. log-execution
+  if (opts.execution) {
+    if (opts.optimize) {  // pre-optimize
+      // TODO: converge?
+      module.optimize();
+    }
+    module.runPasses(["flatten", "log-execution"]);
+  }
+
+  // 4. final optimize
   if (opts.optimize) {
-    log("Optimizing...")
     module.optimize();
   }
 
   if (!module.validate()) {
-    fatal(`Validation failed.`);
+    fatal(`Validation failed`);
   }
   let result = module.emitBinary();
   module.dispose();
@@ -193,7 +273,7 @@ async function instrument(binary, opts)
 
 async function execute(binary, opts)
 {
-  const trace = fs.createWriteStream(opts.output);
+  const trace = fs.createWriteStream(".wasm-trace.raw");
 
   function traceMemory(name) {
     return (id, val) => {
@@ -214,6 +294,17 @@ async function execute(binary, opts)
       log_execution: function (id) {
         trace.write(`exec: ${id}\n`);
       },
+
+      log_exec_enter: function (id, func) {
+        trace.write(`enter: ${id} ${func}\n`);
+      },
+      log_exec_exit: function (id, func) {
+        trace.write(`exit: ${id} ${func}\n`);
+      },
+      log_exec_loop: function (id) {
+        trace.write(`loop: ${id}\n`);
+      },
+
       load_ptr: function (id, align, offset, address) {
         trace.write(`load ptr: ${id} ${align} ${offset} ${address}\n`);
         return address;
@@ -249,7 +340,7 @@ async function execute(binary, opts)
     const wasi = new Wasi.WASI({
       args:     opts._,
       env:      {},
-      preopens: { ".": "./" },
+      preopens: { "./": "./" },
       bindings: Object.assign({}, Node.default, { fs }),
     });
 
@@ -271,8 +362,108 @@ async function execute(binary, opts)
     let result = instance.exports[opts.invoke](...opts.args);
     log(`Result: ${result}`);
   } else {
-    fatal("Cannot execute: entry function not specified.")
+    fatal("Cannot execute: entry function not specified")
   }
 
   trace.end();
 }
+
+async function post_process(binary, raw, opts)
+{
+  log(`Processing...`)
+
+  const wasmInfo = await analyze_wasm(binary);
+
+  if (!wasmInfo.isInstrumented) {
+    fatal("Processing raw trace file requires input of corresponding pre-instrumented wasm file")
+  }
+
+  const trace = fs.createWriteStream(opts.output);
+
+  const ctx = {
+    exec_depth: 0,
+    mem: {}
+  }
+
+  function indent(id) {
+    return id.toString().padStart(6) + ' | ' + '  '.repeat(Math.max(0, ctx.exec_depth));
+  }
+
+  function getFunctionName(i) {
+    if (wasmInfo.functions[i]) {
+      return wasmInfo.functions[i].name;
+    } else {
+      return '$'+i;
+    }
+  }
+
+  const traceMemory = (name) => ({
+    [name]: (id, val) => {
+      assert(ctx.mem[id]);
+      trace.write(indent(id)+`${name}: ${ctx.mem[id].address}+${ctx.mem[id].offset} ${val}\n`);
+      ctx.mem[id] = undefined;
+      return val;
+    }
+  });
+
+  const traceLocal = (name) => ({
+    [name]: (id, local, val) => {
+      trace.write(indent(id)+`${name}: ${local} ${val}\n`);
+      return val;
+    }
+  });
+
+  const lookup = {
+    "exec": function (id) {
+      trace.write(indent(id)+`exec\n`);
+    },
+
+    "enter": function (id, func) {
+      trace.write(indent(id)+`enter ${getFunctionName(func)} {\n`);
+      ctx.exec_depth+=1;
+    },
+    "exit": function (id, func) {
+      ctx.exec_depth-=1;
+      trace.write(indent(id)+`}\n`);
+    },
+    "loop": function (id) {
+      trace.write(indent(id)+`loop\n`);
+    },
+
+    "load ptr": function (id, align, offset, address) {
+      assert.equal(ctx.mem[id], undefined)
+      ctx.mem[id] = { align, offset, address }
+    },
+    "store ptr": function (id, align, offset, address) {
+      assert.equal(ctx.mem[id], undefined)
+      ctx.mem[id] = { align, offset, address }
+    },
+
+    ...traceMemory( "load i32"),
+    ...traceMemory("store i32"),
+    ...traceMemory( "load i64"),
+    ...traceMemory("store i64"),
+    ...traceMemory( "load f32"),
+    ...traceMemory("store f32"),
+    ...traceMemory( "load f64"),
+    ...traceMemory("store f64"),
+
+    ...traceLocal("get i32"),
+    ...traceLocal("set i32"),
+    ...traceLocal("get i64"),
+    ...traceLocal("set i64"),
+    ...traceLocal("get f32"),
+    ...traceLocal("set f32"),
+    ...traceLocal("get f64"),
+    ...traceLocal("set f64"),
+  };
+
+  for await (const line of readLines(raw)) {
+    let elements = line.split(":").map((s) => s.trim());
+    const cmd = elements[0];
+    const args = elements[1].split(" ").map((s) => parseInt(s, 10));
+
+    lookup[cmd](...args);
+  }
+}
+
